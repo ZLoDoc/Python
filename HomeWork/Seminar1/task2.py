cc82a58edc7c599bdd5252593875af0294e886be
ZLoDoc <zlodoc@rambler.ru>
2022-09-25 08:34:57 +0300

    Task2 completed

--- HomeWork/Seminar1/task2.py
+++ HomeWork/Seminar1/task2.py
@@ -1,14 +1,21 @@
 # 2. Напишите программу для. проверки истинности утверждения 
 #    ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = 234.324
-y = 234
-z = 35
+X = [True, False]
+Y = [True, False]
+Z = [True, False]
 
-expression1 = not(x or y or z) 
-expression2 = not x and not y and not z
-print(f'expression1 = {expression1}, expression2 = {expression2}')
-if expression1 == expression2:
-    print('Утверждение истино')
-else:
-    print('Утверждение ложно')
+for x in X :
+    for y in Y:
+        for z in Z:
+            expression1 = not(x or y or z) 
+            expression2 = (not x) and (not y) and (not z)
+            print('      ')
+            #print(f'x = {x} y = {y} z = {z}')
+            print(f'expression1 = {expression1}, expression2 = {expression2}')
+            #print('      ')
+            if expression1 == expression2:
+                print(f'При Х = {x} Y = {y} Z = {z} - утверждение  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - истинно')
+            else:
+                print(f'При Х = {x} Y = {y} Z = {z} - утверждение  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - ложно')
+            print('____________________________________________________________________')
