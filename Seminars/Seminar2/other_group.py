df875b883c8d30c6eeb505b148fd2440fbda21e9
ZloDoc <zlodoc@rambler.ru>
2022-09-27 17:58:33 +0300

    group2

--- Seminars/Seminar2/other_group.py
+++ Seminars/Seminar2/other_group.py
@@ -6,3 +6,20 @@
 # n = int(input())
 # for i in range (n):
 #     print(3*(i+1)+1)
+string1 = input('Введите первую строку: ')
+string2 = input('Введите вторую строку: ')
+result = 0
+if len(string1)>len(string2):
+    for i in range (len(string1) - len(string2) + 1):
+        if string2 in string1[i:i+len(string2)]:
+            result += 1
+            s=0
+else:
+    for i in range (len(string2) - len(string1) + 1):
+        if string1 in string2[i:i+len(string1)]:
+            result += 1
+            s=1
+if s == 0:
+    print(f'Строка "{string2}" встречается в строке "{string1}" {result} раз')
+elif s==1:
+    print(f'Строка "{string1}" встречается в строке "{string2}" {result} раз')    
