06c3f42b0c1e909918e89af1963c2ebd3cc5235d
ZLoDoc <zlodoc@rambler.ru>
2022-09-28 21:40:42 +0300

    Task5 completed

--- HomeWork/Seminar2/Task5.py
+++ HomeWork/Seminar2/Task5.py
@@ -2,3 +2,18 @@
 # random.randint(2, 6)
 # random.randrange(2, 6)
 # '''
+# stroka=[]
+import random
+
+stroka=list(input(f'Введите список : '))
+print(stroka)
+# index1=len(stroka)
+for i in range (100):
+    index_1 = random.randrange(len(stroka))
+    index_2 = random.randrange(len(stroka))
+    temp = stroka[index_1]
+    stroka[index_1]=stroka[index_2]
+    stroka[index_2]=temp
+print(stroka)
+
+
