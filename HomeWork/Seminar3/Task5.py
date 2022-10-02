98ff80c3699ae50bb150d6f9c1c3f4758ae272b0
ZLoDoc <zlodoc@rambler.ru>
2022-10-03 00:17:37 +0300

    Homework3 Task5  complited

--- HomeWork/Seminar3/Task5.py
+++ HomeWork/Seminar3/Task5.py
@@ -6,8 +6,28 @@
 # - для k = 8 список будет выглядеть так: 
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
 # [Негафибоначчи](https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D0%B3%D0%B0%D1%84%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8#:~:text=%D0%92%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B5%2C%20%D1%87%D0%B8%D1%81%D0%BB%D0%B0%20%D0%BD%D0%B5%D0%B3%D0%B0%D1%84%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8%20%E2%80%94%20%D0%BE%D1%82%D1%80%D0%B8%D1%86%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%20%D0%B8%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D1%8B%20%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20%D1%87%D0%B8%D1%81%D0%B5%D0%BB%20%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8.)
-# Fn = F(n+2)−F(n+1) или  F−n = (−1)n+1Fn.
+# Fn = F(n+2)−F(n+1) или  F−n = (−1)**n+1 * Fn.
 
-n = insert('Задайте число : ')
-for i in range (n*2+1):
-    
+def Fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    else: 
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+def Negafibonacci(n):
+    if n == -1:
+        return 1
+    elif n == -2:
+        return -1
+    else:
+        return Negafibonacci(n+2) - Negafibonacci(n+1)
+
+
+num = int(input('Задайте число : '))
+fibonacchi_list = []
+for i in range(-num,0):    
+    fibonacchi_list.append(Negafibonacci(i))
+fibonacchi_list.append(0)   
+for i in range(1,num+1):    
+    fibonacchi_list.append(Fibonacci(i))
+print(fibonacchi_list)    
