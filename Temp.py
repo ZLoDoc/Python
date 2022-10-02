8ed01d863b1dd33e4c19c1bbd9408864445eaa5c
ZLoDoc <zlodoc@rambler.ru>
2022-10-03 00:35:05 +0300

    Commit temp

--- Temp.py
+++ Temp.py
@@ -1,20 +1,33 @@
-def lenght(n): # вычисляем длину двоичного числа
-    res = 0
-    if n == 0:
-        return 1
-    while n > 0:
-        res+=1
-        n = int(n / 2)
-    return res
+# Задайте число. Составьте список чисел Фибоначчи, 
+# в том числе для отрицательных индексов.
 
-n = int(input('Введите число для конвертации :'))
-a = n 
-b = n  
-binary_list = ''
-result_list = ''
-for i in range (int(lenght(n))):
-    b = (a%2)
-    a = int(a/2)
-    binary_list.append(b)
-result_list = binary_list(::-1)
-print(f'{n} = {str(result_list)}')
+# Пример:
+
+# - для k = 8 список будет выглядеть так: 
+# [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
+# [Негафибоначчи](https://ru.wikipedia.org/wiki/%D0%9D%D0%B5%D0%B3%D0%B0%D1%84%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8#:~:text=%D0%92%20%D0%BC%D0%B0%D1%82%D0%B5%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B5%2C%20%D1%87%D0%B8%D1%81%D0%BB%D0%B0%20%D0%BD%D0%B5%D0%B3%D0%B0%D1%84%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8%20%E2%80%94%20%D0%BE%D1%82%D1%80%D0%B8%D1%86%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%20%D0%B8%D0%BD%D0%B4%D0%B5%D0%BA%D1%81%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D1%8B%20%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20%D1%87%D0%B8%D1%81%D0%B5%D0%BB%20%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8.)
+# Fn = F(n+2)−F(n+1) или  F−n = (−1)**n+1 * Fn.
+
+def Fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    else: 
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
+def Negafibonacci(n):
+    if n == -1:
+        return 1
+    elif n == -2:
+        return -1
+    else:
+        return Negafibonacci(n+2) - Negafibonacci(n+1)
+
+
+num = int(input('Задайте число : '))
+fibonacchi_list = []
+for i in range(-num,0):    
+    fibonacchi_list.append(Negafibonacci(i))
+fibonacchi_list.append(0)   
+for i in range(1,num):    
+    fibonacchi_list.append(Fibonacci(i))
+print(fibonacchi_list) 
