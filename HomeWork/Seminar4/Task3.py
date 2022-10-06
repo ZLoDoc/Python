77194e9a02cb285f722776117165175c08b4daa5
ZLoDoc <zlodoc@rambler.ru>
2022-10-06 20:14:51 +0300

    Workshop4 Task 3 Completed

--- HomeWork/Seminar4/Task3.py
+++ HomeWork/Seminar4/Task3.py
@@ -2,27 +2,15 @@
 # Напишите программу, которая выведет список неповторяющихся
 # элементов исходной последовательности.
 # 1 2 2 1 3 4 5 6 6 7 4 -> 3 5 7
-import re
   
 num = input('Введите ряд чисел: ')
-num = (re.split(" ,|, | |;|,|\n", num))
-# for i in range(len(num)):
-count = 0
-res_list=[]
+result = []
 for i in range(len(num)):
-    print(f'num i{i} = {num[i]}')
+    count = 1   
     for j in range(len(num)):
-        if num[j]==num[i]:
-            count = count + 1
+        if num[i] == num[j] and i != j:
+            count = 0
+            break
     if count == 1:
-        res_list.append(num[i])
-
-    # print(f'num i{i} = {num[i]}')
-        print(f'   num j{j} = {num[j]}')
-        print('----------------------------')
-    
-    # print('----------------------------')
-#         if num[i] == num[j]:
-#             count = count+1
-#     if count == 1:
-#         result = result + num[i]
+        result.append(num[i])
+print(f'Список неповторяющихся элементов -  {result}')
--- Temp.py
+++
@@ -1,6 +0,0 @@
-
-import re
-str_ = 'qwer, dfdfdf ,erwrer dfsdfd ,fsdfsdfs'
-# a = (str_.split(', '))
-# print(a)
-print(re.split(" ,|, | |;|,|\n", str_))
