b473ecd483afdc7aba389a0006289d61f6e3280a
ZloDoc <zlodoc@rambler.ru>
2022-10-06 18:04:15 +0300

    Workshop 4 Task3 in progress

--- HomeWork/Seminar4/Task3.py
+++ HomeWork/Seminar4/Task3.py
@@ -2,3 +2,27 @@
 # Напишите программу, которая выведет список неповторяющихся
 # элементов исходной последовательности.
 # 1 2 2 1 3 4 5 6 6 7 4 -> 3 5 7
+import re
+  
+num = input('Введите ряд чисел: ')
+num = (re.split(" ,|, | |;|,|\n", num))
+# for i in range(len(num)):
+count = 0
+res_list=[]
+for i in range(len(num)):
+    print(f'num i{i} = {num[i]}')
+    for j in range(len(num)):
+        if num[j]==num[i]:
+            count = count + 1
+    if count == 1:
+        res_list.append(num[i])
+
+    # print(f'num i{i} = {num[i]}')
+        print(f'   num j{j} = {num[j]}')
+        print('----------------------------')
+    
+    # print('----------------------------')
+#         if num[i] == num[j]:
+#             count = count+1
+#     if count == 1:
+#         result = result + num[i]
--- Temp.py
+++ Temp.py
@@ -1,23 +1,6 @@
-print(help(binary_searsh))
-# import time
 
-# tic = time.perf_counter()
-# def pi():
-#     k = float(input("Введите точность от 0,1 до 0,0000000001  :  "))
-#     d = int(1/k) * 10
-#     print(d)
-#     count = -1
-#     number = 1
-#     while number < d:
-#         number *= 10
-#         count += 1
-#     pi_for = 1 - 1 / 3 + 1 / 5 - 1 / 7 + 1 / 9
-#     for i in range(5, d):
-#         pi_for = pi_for + (1 / (2 * i + 1)) * (-1) ** i
-#     pi_ = pi_for * 4
-#     print(f'Точность  pi = {pi_} до {count} знака после запятой')
-
-
-# pi()
-# toc = time.perf_counter()
-# print(f"Вычисление заняло {toc - tic:0.4f} секунд")
+import re
+str_ = 'qwer, dfdfdf ,erwrer dfsdfd ,fsdfsdfs'
+# a = (str_.split(', '))
+# print(a)
+print(re.split(" ,|, | |;|,|\n", str_))
