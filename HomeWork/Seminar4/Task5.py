723121d8bd3369d0db605abec083e37073bea8b1
ZLoDoc <zlodoc@rambler.ru>
2022-10-11 21:32:39 +0300

    Task5

--- HomeWork/Seminar4/Task5.py
+++ HomeWork/Seminar4/Task5.py
@@ -6,7 +6,7 @@
 
 # 8x^2 + 1x - 14
 
-НЕТ ЧЕТКОГО АЛГОРИТМА, ЗАДАЧА НЕ РЕШЕНА
+# НЕТ ЧЕТКОГО АЛГОРИТМА, ЗАДАЧА НЕ РЕШЕНА
 
 f = open('file1.txt', 'r')
 polinom_1 = list(f.read().split(' '))
@@ -16,17 +16,15 @@ f = open('file2.txt', 'r')
 polinom_2 = list(f.read().split(' '))
 f.close()
 
-print(polinom_1)
-print(polinom_2)
-print(polinom_1[0])
-print(len(polinom_1))
+# print(polinom_1)
+# print(polinom_2)
+# print(len(polinom_1))
 
-print(f'{(polinom_1[0][len(polinom_1[0])-1])} = {(polinom_2[0][len(polinom_2[0])-1])}') #Проверяем степень монома 
-if (polinom_1[0][len(polinom_1[0])-1]) == (polinom_2[0][len(polinom_2[0])-1]) and : #Проверяем степень монома 
-    for i in range(len(polinom_1)): # Этим циклом хочу сложить коэффициенты мономов
-        for j in range(len(polinom_2)):
-            if len(polinom_1[i]) == len (polinom_2[j]):# Проверяю не стоит ли знак в первой ячейке
-                
+print(f'степень 1 = {(polinom_1[0][len(polinom_1[0])-1])} \nстепень 2 = {(polinom_2[0][len(polinom_2[0])-1])}') #Проверяем степень монома 
+print(f'1 моном 1-го полинома - {polinom_1[0]}')
+print(f'1 моном 2-го полинома - {polinom_2[0]}')
+if (polinom_1[0][len(polinom_1[0])-1]) == (polinom_2[0][len(polinom_2[0])-1]) : #Проверяем степень монома 
+ #если минус ?               
                 print((polinom_1[0])[0])
                 print((polinom_2[0])[0])
             
