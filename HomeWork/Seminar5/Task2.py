d2b346001efd3e71df03e06f6deca910749c1e96
ZLoDoc <zlodoc@rambler.ru>
2022-10-15 22:03:04 +0300

    Homework 5 Task2 Completed

--- HomeWork/Seminar5/Task2.py
+++ HomeWork/Seminar5/Task2.py
@@ -9,38 +9,124 @@
 #   b) Подумайте как наделить бота ""интеллектом""
 
 import random
-# player_1name = input('Имя первого игрока ?')
-player_1name ='Вася'
-print(f'\nЗдравствуй {player_1name}')
-# player_2name = input('Имя второго игрока ?')
-player_2name = 'Катя'
-print(f'\nЗдравствуй {player_2name}')
-саndy = 30
-print(f'\nна столе {саndy} конфет')
+def name_choice_step():
+   
+    con = dict()
+    first = int(random.randrange(0,2))
+    con["first"] = first
+    candy = int(input('Сколько конфет на кону? : '))
+    con['candy'] = candy
+    chose = 1
+    while chose > 0: 
+        choice = int(input('\n\n\n\n\nвыбири вариант игры: 1 - с человеком, 2 - с компом, 3 - c непобедимым: '))
+        con['choice'] = choice
+        if 0 >= choice or choice > 3: 
+            continue
+        else: chose = chose-1
+      
+        player_1name = input('Имя первого игрока ?: ')
+        # player_1name ='Вася'
+                
+        if choice == 1:    
+            player_2name = input('Имя второго игрока ?: ')
+            # player_2name = 'Катя'        
+        if choice == 2:    
+            player_2name = 'Comp'
+        if choice == 3:    
+            player_2name = 'Bot'
+                    
+        print(f'\nПриветствую вас {player_1name} и {player_2name} !!!!')
+        if first == 1:
+            print(f'\nПервым ходит {player_1name}')
+        if first == 0: 
+            temp = player_1name
+            player_1name = player_2name
+            player_2name = temp
+            print(f'\nПервым ходит {player_1name}')
+        con['player_1name'] = player_1name
+        con['player_2name'] = player_2name     
+    return (con)
 
-while саndy >= 0:
-    player_1 = int(input(f'\n\t {player_1name} возьми не больше 28 конфет :'))
-    if 0 < player_1 <= 28 : 
-        саndy = саndy - player_1
-        if саndy <=0:
-            print(f'{player_1name} проиграл')
-            break
-
-        print(f'\nна столе {саndy} конфет')
-    else:
-        continue
-    player_2 = int(input(f'\n\t {player_2name} возьми не больше 28 конфет :'))
-    if 0 < player_2 <= 28 : 
-        саndy = саndy - player_2
-        if саndy <=0:
-            print(f'{player_2name} проиграл')
-            break
-        print(f'\nна столе {саndy} конфет')
-    else:
-        continue
+def players_algorithm(con):
+    flag = 1
+    candy = con["candy"]
+    while candy >= 0:
+        print(f'на столе {candy} конфет')
+        if flag == 1:
+                player = con["player_1name"]
+        if flag == 0:
+                player = con["player_2name"]
+        step = int(input(f'\t {player} возьми не больше 28 конфет :'))
+        if 0 < step <= 28 : 
+            candy = candy - step
+            if candy <=0:
+                return player
+            if flag == 1 : flag = 0
+            else: flag = 1
     
-        # else:
-        #     print(f'\nна столе {саndy} конфета')
-        #     comp = int(random.randrange(0,5))
-        #     print(f'\n\tя взял {comp} конфет')
-        #     саndy = саndy - comp
+        else: 
+            continue
+# ____________________________________________________________________________,
+
+def comp_algorithm(con):
+    flag = 1
+    candy = con["candy"]
+    while candy >= 0:
+        print(f'на столе {candy} конфет')
+        if flag == 1:
+                player = con["player_1name"]
+        if flag == 0:
+                player = con["player_2name"]
+        if player == 'Comp':
+            step = int(random.randrange(1,29))
+            print (f'\t{player} взял {step} конфет')
+        if player != 'Comp':            
+            step = int(input(f'\n\t {player} возьми не больше 28 конфет :'))
+        if 0 < step <= 28 : 
+            candy = candy - step
+            if candy <=0:
+                return player
+            if flag == 1 : flag = 0
+            else: flag = 1
+        
+        else: 
+            continue
+# ____________________________________________________________________________,
+def bot_algorithm(con):
+    flag = 1
+    candy = con["candy"]
+    first_step = 0
+    while candy >= 0:
+        print(f'на столе {candy} конфет')
+        
+        if flag == 1:
+            player = con["player_1name"]
+        if flag == 0:
+            player = con["player_2name"]
+        
+        if player == 'Bot':
+            if first_step == 0:
+                step = (candy - (int(candy / 29) * 29))-1                
+                first_step = 1
+            elif first_step != 0:
+                step = 29 - player_step
+            print (f'\t{player} взял {step} конфет')
+        
+        if player != 'Bot':            
+            step = int(input(f'\n\t {player} возьми не больше 28 конфет :'))
+            player_step = step 
+
+        if 0 < step <= 28 : 
+            candy = candy - step
+            if candy <=0:
+                return player
+            if flag == 1 : flag = 0
+            else: flag = 1
+        
+        else: 
+            continue
+        # ____________________________________________________________________________,
+a = (name_choice_step())
+if a["choice"] == 1: print(f'Игрок {players_algorithm(a)} проиграл !')
+if a["choice"] == 2: print(f'Игрок {comp_algorithm(a)} проиграл !')
+if a["choice"] == 3: print(f'Игрок {bot_algorithm(a)} проиграл !')        
--- Temp2.py
+++ Temp2.py
@@ -1,13 +1,38 @@
-text = '123456789'
-i=0
-while i <= (len(text)):
-    if i == 3:
-        i=5
-    print(text[i])
-    i= i + 1 
+def bot_algorithm():
+    flag = 1
+    candy = 100
+    first_step = 0
+    while candy >= 0:
+        print(f'на столе {candy} конфет')
+        
+        if flag == 1:
+            player = 'aaaaaaa'
+        if flag == 0:
+            player = 'Bot'
+        
+        if player == 'Bot':
+            if first_step == 0:
+                step = (candy - (int(candy / 29) * 29))-1
+                print(f'первый ход{step}')
+                first_step = 1
+            elif first_step != 0:
+                step = 29 - player_step
+            print (f'{player} взял {step} конфет')
+        
+        if player != 'Bot':            
+            step = int(input(f'\n\t {player} возьми не больше 28 конфет :'))
+            player_step = step 
 
-# '\nИ редакабвтора и поэта не стабволько поразабвило то,' 
-#     ' что наабвшлась в портабвсигаре имабвенно «Наабвша марка», '
-#     'сколькабво саабвм портсиабвгар. Он бабвыл громаднабвых размеабвров, '
-#     'червонабвного золабвота, и набва крыабвшке егабво при открыабввании '
-#     'сверкнабвул сиабвним и беабвлым огабвнем бриллиабвантовый треугоабвльник.\n'
+        if 0 < step <= 28 : 
+            candy = candy - step
+            if candy <=0:
+                # print(f'{player} проиграл')
+                return player                
+
+            # print(f'\nна столе {candy} конфет')
+            if flag == 1 : flag = 0
+            else: flag = 1
+        
+        else: 
+            continue
+print(f'Игрок {bot_algorithm()} проиграл !')
