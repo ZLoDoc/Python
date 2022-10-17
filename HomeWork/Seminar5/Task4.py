e7c5a26c11dc377e1aed29624e7d4fa0b5a8b991
ZloDoc <zlodoc@rambler.ru>
2022-10-17 18:00:01 +0300

    Homework5Task4 Correct

--- HomeWork/Seminar5/Task4.py
+++ HomeWork/Seminar5/Task4.py
@@ -1,17 +1,31 @@
 # Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных. 
-text = 'aaaaabbbbbbccccccchhhhhh'
-pos = 0
-nachalo = pos
-count = 0
-result_string = ''
-for i in range(len(text)):
-    if text[pos] == text[nachalo]:
-        count = count+1
-        pos = pos + 1
-    elif text[pos] != text[nachalo]:
-        result_string = result_string + text[nachalo] + str(count)
-        nachalo = i
-        pos = pos + 1
-        count = 1
-result_string = result_string + text[nachalo] + str(count)        
-print(result_string)        
+# from curses.ascii import isdigit
+
+
+# text = 'aaaaabbbbbbccccccchhhhhh'
+# pos = 0
+# nachalo = pos
+# count = 0
+# result_string = ''
+# for i in range(len(text)):
+#     if text[pos] == text[nachalo]:
+#         count = count+1
+#         pos = pos + 1
+#     elif text[pos] != text[nachalo]:
+#         result_string = result_string + text[nachalo] + str(count)
+#         nachalo = i
+#         pos = pos + 1
+#         count = 1
+# result_string = result_string + text[nachalo] + str(count)        
+# print(result_string)        
+
+from curses.ascii import isdigit
+
+
+code = 'a5b6c7h6'
+# code = list(map(int,code))
+# print(code)
+for i in range(len(code)):
+    if code[i].isdigit:
+        for j
+    # if code[i].isdigit:
--- Seminars/Seminar5/Seminar5.py
+++ Seminars/Seminar5/Seminar5.py
@@ -28,8 +28,8 @@
 #             a.append(n**3)
 # print(a)    
 
-b = [n*n if not n % 2 else n**3 for n in range(11) if n % 5 !=0] 
-print(b)
+# b = [n*n if not n % 2 else n**3 for n in range(11) if n % 5 !=0] 
+# print(b)
 # _______________________________________________
 # # MAP # FILTER  # ZIP
 
@@ -43,3 +43,42 @@ print(b)
 # c = (False, True, None)
 # for item in zip(a,b,c):
 #     print(f'ZIP {item}')
+
+# 1. В файле находится N натуральных чисел, записанных через пробел.
+# Среди чисел не хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1].
+# Найдите это число.
+
+# f = open('file.txt', 'r')
+# a = list(f.read().split(' '))
+# # sequence = str(f.read())
+# f.close()
+# print(len(a))
+
+# result = list(map(int, a))
+# print(result)
+
+# for i in range(1,len(result)):
+#     if result[i]-1 != result[i-1]:
+#         print(f'Это число {result[i]-1}')
+# 2. Напишите программу, удаляющую из текста все слова,
+#     содержащие "абв". <- filter
+
+# text = ('абв проверка связи абв ищу редиску')
+# text= text.split(' ')
+# print(text)
+
+# result_text = list(filter(lambda x: x != 'абв' , text))
+# result_text = " ".join(result_text)
+# print(result_text)
+
+# 3. Дан список чисел. Создайте список, в который попадают числа, 
+# описываемые возрастающую последовательность.
+# Порядок элементов менять нельзя.
+
+# [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.
+
+value = [1, 5, 2, 3, 4, 6, 1, 7]
+value = list(map(int,value))
+result =[]
+
+print(value)
