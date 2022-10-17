8673b2ea61233206eb79bb8792325feaafa800ea
ZLoDoc <zlodoc@rambler.ru>
2022-10-17 10:47:34 +0300

    Homework5 Task5

--- HomeWork/Seminar5/Task5.py
+++ HomeWork/Seminar5/Task5.py
@@ -4,15 +4,24 @@
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
 
 import random
+
+f = open('list.txt', 'r')
+sequence = list(f.read().split(' '))
+f.close()
+
+print(s1)
 i = 0
-sequence = [1, 5, 2, 3, 4, 6, 1, 7]
+# sequence = [1, 5, 2, 3, 4, 6, 1, 7]
 result_list = []
 while i < (len(sequence)):
     if i == 0: 
         result_list.append(sequence[i])
-        i = random.randrange(i,len(sequence)) #выбираю индекс второго значения
-        print(i)
+        i = random.randrange(i,len(sequence)-1) #выбираю индекс второго значения        
     elif sequence[i] > result_list[(len(result_list))-1]: result_list.append(sequence[i])
     i = i + 1
-print(result_list)    
+result = " ".join(result_list)
+print(sequence)   
+print(result) 
+with open('list_res.txt', 'a') as data:
+    data.write(f'{" ".join(result_list)}\n')   
 
